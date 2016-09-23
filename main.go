1f238b81e6994715ac1da84fada4da8094b9f52c
zouyu <zouyu@shiqichuban.com>
2016-09-23 21:36:14 +0800

    add qzone limitation.

--- main.go
+++ main.go
@@ -13,6 +13,26 @@ func main() {
 		MaxConn: 10,
 		MaxRps:  80,
 	}
+	m["qzoneuser"] = &proxy.TargetInfo{
+		Target:  "http://users.qzone.qq.com",
+		MaxConn: 10,
+		MaxRps:  80,
+	}
+	m["qzoneb1"] = &proxy.TargetInfo{
+		Target:  "http://b1.qzone.qq.com",
+		MaxConn: 10,
+		MaxRps:  80,
+	}
+	m["qzoneb11"] = &proxy.TargetInfo{
+		Target:  "http://b11.qzone.qq.com",
+		MaxConn: 10,
+		MaxRps:  80,
+	}
+	m["qqtaotao"] = &proxy.TargetInfo{
+		Target:  "http://taotao.qq.com",
+		MaxConn: 10,
+		MaxRps:  80,
+	}
 	p, _ := proxy.NewGtsProxy(m)
 	p.AddIps("60.205.127.147", "101.201.53.131", "127.0.0.1")
 	p.Run()
